c0a99087ed26702089ecde3786d1c80b895d8de3
ximenajav <93632916+ximenajav@users.noreply.github.com>
2021-11-07 22:22:33 -0500

    Add files via upload

---
+++ Javadpour Proposal.R
@@ -0,0 +1,39 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+The title of our project will likely be "The Best Battleship There Ever Was"
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+Our project will consist of a game of Battleship, where two players call "shots" at the other player's ships. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+Our software will allow two players to play a game of Battleship with the end goal of one player destroying their opponents ships. It will include features prompt user what space user wants to attack and mark it. It will then reveal if there is a ship there and have a graphic with an explosion. It is essential that the program keeps track of the results of each simulation. This will be executed using Pygame.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+Not applicable 
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Diana Volynsky, diana.volynsky@temple.edu, Joanne Nichols  
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+A GOOD outcome for our final project is for the game to work/run properly without any malfunctions. We will accomplish all of the rules of Battleship working when running the program.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+A BETTER outcome for our final project is for the game to have graphics that add creativity to our project. We will also aim to create different size ships to add our own twist. 
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+A BEST outcome for our final project is to create moving graphics and possibly create animations so that our game is unique and not only fun to play, but also fun to look at. Additionally, we hope to accomplish a working game where more than two people can play. 
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+Our next steps will consist of beginning to write the code for our game of Battleship. Ximena and I will split up the code so that we equally contribute to the project. We will focus on writing the code and for the code to run before beginning to make any graphics as that is the most important aspect of this project. Since we do not have as much experience with making graphics, we plan to set aside some extra time to figure out which graphics we want to include and how to include them. In order to be successfull in making the graphics unique, we need to acquire some skills by researching the different options we have and then looking at examples. Depending on the number of graphics we implement, Ximena and I will also be splitting up each graphic so that equally contribute to the project so that we both gain knowledge on how to not only create code for this game, but also how to code graphics. We also must research if there are any specific files we need to import. 
