a710b37c2e463ea239a90369131606dca6b3150c
ximenajav <93632916+ximenajav@users.noreply.github.com>
2021-11-20 21:29:06 -0500

    Add files via upload

---
+++ status.R
@@ -0,0 +1,29 @@
+# Status Report
+
+#### Your name
+
+Ximena Javadpour
+
+#### Your section leader's name
+
+Joanne Nichols
+
+#### Project title
+
+Battleship
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+So far we have worked on the basic code for the battleship game and have set up a working game of battleship though it does not yet have any fancy graphics or twists.
+
+#### What have you not done for your project yet?
+
+We have not yet added anything additional to the bare bones of the battleship game.
+
+#### What problems, if any, have you encountered?
+
+We've had to work through a few bugs with the code, but we are having the most issues with making the game have a twist do something extra to the basic game. 
